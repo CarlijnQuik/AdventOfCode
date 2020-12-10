8a998c70de91450a0979d9b87d7f7040dd3fa63e
cquik <cquik@deloitte.nl>
2020-12-10 20:18:08 +0100

    solved day 5

--- Day5.py
+++ Day5.py
@@ -5,54 +5,39 @@ for boarding_pass in f:
     boarding_passes.append(boarding_pass.strip())
 f.close()
 
+
+def determine_seat(boarding_pass):
+    min_row = 0
+    max_row = int(128)
+    min_column = 0
+    max_column = int(8)
+    for letter in boarding_pass:
+        if letter == "F":
+            max_row = int((max_row+min_row)/2)
+        elif letter == "B":
+            min_row = int((max_row+min_row)/2)
+        elif letter == "L":
+            max_column = int((max_column+min_column)/2)
+        elif letter == "R":
+            min_column = int((max_column+min_column)/2)
+        else:
+            break
+    return {"row": min_row, "column": min_column, "pass": boarding_pass, "id": (min_row*8)+min_column}
+    
 def get_seat_ids():
-    boarding_pass_seats = {}
     seat_ids = []
     for boarding_pass in boarding_passes:
-        min_row = 0
-        max_row = int(128)
-        min_column = 0
-        max_column = int(8)
-        for letter in boarding_pass:
-            if letter == "F":
-                max_row = int((max_row+min_row)/2)
-            elif letter == "B":
-                min_row = int((max_row+min_row)/2)
-            elif letter == "L":
-                max_column = int((max_column+min_column)/2)
-            elif letter == "R":
-                min_column = int((max_column+min_column)/2)
-            else:
-                break
-            
-        boarding_pass_seats[(min_row*8)+min_column] = {"row": min_row, "column": min_column, "pass": boarding_pass}
-        seat_ids.append((min_row*8)+min_column)
-    return boarding_pass_seats,seat_ids
-
-seat_ids = get_seat_ids()[1]
-seat_ids.sort()
-
-# print("Part 1: ", seat_ids[len(seat_ids)-1])
-
-def possible_seats():
-    row = 1
-    max_row = int(126)
-    max_column = int(8)
-    seat_ids = []
-    while row < max_row:
-        column = 0
-        while column < max_column:
-            seat_ids.append((row*8)+column)
-            column += 1
-        row += 1
-    
+        seat = determine_seat(boarding_pass)
+        seat_ids.append(seat["id"])
     return seat_ids
 
-# Part 2
-possible_seats = possible_seats()
-empty_seat_ids = list(set(possible_seats) - set(seat_ids))
+# Part 1
+seat_ids = get_seat_ids()
+print("Part 1: ", max(seat_ids))
 
-print(empty_seat_ids)
+# Part 2
+my_seat = set(range(min(seat_ids), max(seat_ids))) - set(seat_ids)
+print(my_seat)
 
 
 
